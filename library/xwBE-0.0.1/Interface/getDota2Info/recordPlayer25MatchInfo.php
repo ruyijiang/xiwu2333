fa37aa03a8ca53776f3e7189cae71e84d87645f6
milobluebell <1444828173@qq.com>
2016-08-01 23:39:56 +0800

    add some

--- library/xwBE-0.0.1/Interface/getDota2Info/recordPlayer25MatchInfo.php
+++ library/xwBE-0.0.1/Interface/getDota2Info/recordPlayer25MatchInfo.php
@@ -8,21 +8,30 @@
 require("../../connectDB.php");
 require("../../all.php");
 ?><?php
+
+@$UserDota2Id = $_POST["dota2uid"];
+
+
+
 //header('Content-type: application/json');
 $matchesMapperWeb = new Dota2Api\Mappers\MatchesMapperWeb();
 $matchesMapperWeb->setAccountId(252556081);
-$matchesShortInfo = $matchesMapperWeb->load();
+@$matchesShortInfo = $matchesMapperWeb->load();
 var_dump($matchesShortInfo);
 foreach ($matchesShortInfo as $key=>$matchShortInfo) {
     $matchMapper = new Dota2Api\Mappers\MatchMapperWeb($key);
     $match = $matchMapper->load();
-    $slots = $match->getAllSlots();
+    @$slots = $match->getAllSlots();
     foreach($slots as $slot) {
-        if($slot->get('deaths')==0){
-            echo number_format(($slot->get('kills')+$slot->get('assists'))/1,1)."<br>";
-        }else{
-            echo number_format(($slot->get('kills')+$slot->get('assists'))/$slot->get('deaths'),1)."<br>";
-        }
+        //if($slot->getDataArray){//是dota2uid所对应的玩家
+            echo $$slot->getDataArray;
+            if($slot->get('deaths')==0){
+                echo number_format(($slot->get('kills')+$slot->get('assists'))/1,1)."<br>";
+            }else{
+                echo number_format(($slot->get('kills')+$slot->get('assists'))/$slot->get('deaths'),1)."<br>";
+            }
+
+        //}
     }
 
     /*if ($match) {
