f073205873f16592bac35cd4b52bea0fdfae2764
milobluebell <1444828173@qq.com>
2016-08-20 13:50:47 +0800

    finish export dota2_liveness

--- library/xwBE-0.0.1/Interface/getDota2Info/test.php
+++ library/xwBE-0.0.1/Interface/getDota2Info/test.php
@@ -8,7 +8,7 @@
 require("../../connectDB.php");
 require("../../all.php");
 ?><?php
-header('Content-type: application/json');
+//header('Content-type: application/json');
 $weekarray=array("日","一","二","三","四","五","六");
 $weektoday = "周".$weekarray[date("w")];
 
@@ -21,52 +21,67 @@ $dota2_uid = $_GET["dota2_uid"];                            //dota2_uid
 $todayStart = mktime(0,0,0,date("m"),date("d"),date("Y"));  //今天0点UNIX时间
 $todayEnd = $todayStart + 24*60*60-1;                       //今天23:59的UNIX时间
 
+$daysBeforeToday = "";
+$daysBeforeToday_Unix = 0;
 switch (date("w")){
     case "1":
-        $_91daysBeforeToday = date('m/d', strtotime('-91 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-91 days')),date('d', strtotime('-91 days')));
+        $daysBeforeToday = date('m/d', strtotime('-91 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-91 days')),date('d', strtotime('-91 days')));
         break;
     case "2":
-        $_91daysBeforeToday = date('m/d', strtotime('-92 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-92 days')),date('d', strtotime('-92 days')));
+        $daysBeforeToday = date('m/d', strtotime('-92 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-92 days')),date('d', strtotime('-92 days')));
         break;
     case "3":
-        $_91daysBeforeToday = date('m/d', strtotime('-93 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-93 days')),date('d', strtotime('-93 days')));
+        $daysBeforeToday = date('m/d', strtotime('-93 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-93 days')),date('d', strtotime('-93 days')));
         break;
     case "4":
-        $_91daysBeforeToday = date('m/d', strtotime('-94 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-94 days')),date('d', strtotime('-94 days')));
+        $daysBeforeToday = date('m/d', strtotime('-94 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-94 days')),date('d', strtotime('-94 days')));
         break;
     case "5":
-        $_91daysBeforeToday = date('m/d', strtotime('-95 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-95 days')),date('d', strtotime('-95 days')));
+        $daysBeforeToday = date('m/d', strtotime('-95 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-95 days')),date('d', strtotime('-95 days')));
         break;
     case "6":
-        $_91daysBeforeToday = date('m/d', strtotime('-96 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-96 days')),date('d', strtotime('-96 days')));
+        $daysBeforeToday = date('m/d', strtotime('-96 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-96 days')),date('d', strtotime('-96 days')));
         break;
     case "7":
-        $_91daysBeforeToday = date('m/d', strtotime('-97 days'));
-        $_91daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-97 days')),date('d', strtotime('-97 days')));
+        $daysBeforeToday = date('m/d', strtotime('-97 days'));
+        $daysBeforeToday_Unix = mktime(0,0,0,date('m', strtotime('-97 days')),date('d', strtotime('-97 days')));
         break;
 }
 
-$EchoResult = array();                                      //此程序最终要等待json格式化输出的数组
+$EchoResult = array();//此程序最终要等待json格式化输出的数组
+$finaltime = 0;
+do{
 
-$sql = "SELECT id,start_time FROM `info_dota2`.`d2i_matchhistory` WHERE d2id = '$dota2_uid' AND start_time >= '$_91daysBeforeToday_Unix' AND start_time < '$todayStart' ";
-$qry = $db->query($sql);
-@$row = mysqli_num_rows($qry);
+    if($finaltime == 0){
+        $finaltime = $daysBeforeToday_Unix + 86400;
+    }else{
+        $finaltime += 86400;
+    }
+
+    $ISOTime = date('m/d',$finaltime);
+    $EchoResult[$ISOTime] = 0;
+
+
+}while($finaltime < $todayStart);
+
+
+    $sql = "SELECT id,start_time FROM `info_dota2`.`d2i_matchhistory` WHERE d2id = '$dota2_uid' AND start_time >= '$daysBeforeToday_Unix' AND start_time < '$finaltime' ";
+    $qry = $db->query($sql);
     //拿存入数据库的今天之前的比赛数据//从数据库取
     while($row = $qry->fetch_assoc()){
         $ISOTime = date('m/d',$row["start_time"]);
-        if(!isset($EchoResult[$ISOTime]) || empty($EchoResult[$ISOTime])){
+        if(!isset($EchoResult[$ISOTime]) || $EchoResult[$ISOTime] == 0){
             $EchoResult[$ISOTime] = 1;
         }else{
             $EchoResult[$ISOTime]++;
         }
     }
-
 
     //拿今天的数据//从api取
     $last_requested_dota2_match_id = $row["last_requested_dota2_match_id"];
@@ -98,7 +113,7 @@ $qry = $db->query($sql);
                 $qry3 = $db->query($sql3);
                 @$row3 = mysqli_num_rows($qry3);
                 if(!$row3){
-                    $sql2 = "INSERT INTO `info_dota2`.`d2i_matchhistory`(id,match_id,match_seq_num,start_time) VALUES ('',$ThisMatch_id,$ThisMatch_seq_num,$ThisStart_time)";
+                    $sql2 = "INSERT INTO `info_dota2`.`d2i_matchhistory`(id,match_id,match_seq_num,start_time,d2id) VALUES ('',$ThisMatch_id,$ThisMatch_seq_num,$ThisStart_time,$dota2_uid)";
                     $qry2 = $db->query($sql2);
                     if(!$qry2){
                         $status = 0;
