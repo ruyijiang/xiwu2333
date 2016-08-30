b15473946474621bae34c13d4c09f8f1f174634a
milobluebell <1444828173@qq.com>
2016-08-30 17:06:18 +0800

    新建了4个文件，用于调取热门数据

---
+++ library/xwBE-0.0.1/Interface/Community/getHotArticles.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milobluebell
+ * Date: 16-8-30
+ * Time: 上午9:14
+ */
+require("../../connectDB.php");
+require("../../all.php");
+?><?php
+
+$timing = $_GET["timing"];
---
+++ library/xwBE-0.0.1/Interface/Community/getHotTopics.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milobluebell
+ * Date: 16-8-30
+ * Time: 上午9:16
+ */
+require("../../connectDB.php");
+require("../../all.php");
+?><?php
+
+$timing = $_GET["timing"];
+$cate = $_GET["cate"];//分类
