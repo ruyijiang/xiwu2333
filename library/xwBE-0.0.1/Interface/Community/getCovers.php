2c27beb12d64267de9d7b44ba6390947d4be9039
milobluebell <1444828173@qq.com>
2016-08-31 13:55:51 +0800

    创建了getCovers接口，用于community页面的cover封面展示

---
+++ library/xwBE-0.0.1/Interface/Community/getCovers.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milobluebell
+ * Date: 16-8-31
+ * Time: 下午1:54
+ */
+require("../../connectDB.php");
+require("../../all.php");
+?><?php
+
+@$timing = $_GET["timing"];
