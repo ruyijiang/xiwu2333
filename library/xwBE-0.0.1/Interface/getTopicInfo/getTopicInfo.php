e4e9a4cac9475f9a0621a7fe4544dfe1368cc098
milobluebell <1444828173@qq.com>
2016-09-23 17:05:22 +0800

    code some in getTopicInfo.php

--- library/xwBE-0.0.1/Interface/getTopicInfo/getTopicInfo.php
+++ library/xwBE-0.0.1/Interface/getTopicInfo/getTopicInfo.php
@@ -11,3 +11,32 @@ require("../../all.php");
 //header('Content-type: application/json');
 
 @$uid = $_SESSION["uid"];//uid
+
+$content = $_GET["content"];
+
+$a = new _environment();
+$tnow = $a->getTime();//当前datetime - iso时间
+$tnow_stamp = time();//当前datetime - UNIX时间
+
+$status = 0;
+$reminder = "";
+$a = new interfaceResponse();
+
+if($content){
+
+    $sql = "SELECT * FROM topics WHERE topic_id = '$content' OR customed_url = '$content' ";
+    $qry = $db->query($sql);
+    $row_all = mysqli_num_rows($qry);
+    if($row_all > 0){
+
+    }else{
+        //-------------------------------------------------------------------------------->没有找到对应的话题
+    }
+
+
+}else{
+    //------------------------------------------------------------------------------------>缺少关键参数
+    $status = 0;
+    $reminder = "缺少关键参数，无法为您查询";
+    echo $a->normalrespond($status,$reminder);
+}
