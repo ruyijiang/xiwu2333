37922369087f528b2cabdcc96a8cefde3e2dc441
milobluebell <1444828173@qq.com>
2016-10-25 16:55:53 +0800

    添加了检测是否投票的接口。

--- library/xwBE-0.0.1/Interface/checkStatus/check_ballotstatus.php
+++ library/xwBE-0.0.1/Interface/checkStatus/check_ballotstatus.php
@@ -26,9 +26,38 @@ if($topic_id){
 
     $sql = "SELECT topic_id FROM `topics` WHERE topic_id = '$topic_id' ";
     $qry = $db->query($sql);
+    if(mysqli_num_rows($qry) <= 0){
+        //-------------------------------------------------------------------------------->没有找到对应的话题
+        $status = 0;
+        $reminder = "没有找到对应的话题，无法正确执行";
+        echo $a->normalrespond($status,$reminder);
+    }else{
+        $sql2 = "SELECT ballot_id FROM `ballots` WHERE from_uid = '$uid' AND topic_id = '$topic_id' ";
+        $qry2 = $db->query($sql2);
 
-    $sql2 = "SELECT topic_id FROM `comments` WHERE from_uid = '$uid' AND topic_id = '$topic_id' ";
+        if(mysqli_num_rows($qry2) == 1){
+            $rows = $qry2->fetch_assoc();
+            $result_ballot_id = $rows[0]["ballot_id"];
+            //-------------------------------------------------------------------------------->在对应话题下投过票@@@
+            $status = $result_ballot_id;
+            $reminder = "在对应话题下投过票";
+            echo $a->normalrespond($status,$reminder);
 
+        }else if(mysqli_num_rows($qry2) > 1){
+            //-------------------------------------------------------------------------------->用户在对应话题下多次投票
+            $status = 0;
+            $reminder = "查询到多个投票结果";
+            echo $a->normalrespond($status,$reminder);
+
+        }else{
+            //-------------------------------------------------------------------------------->没有在对应话题下投过票
+            $status = 0;
+            $reminder = "没有在对应话题下透过票";
+            echo $a->normalrespond($status,$reminder);
+
+        }
+
+    }
 
 }else{
     //------------------------------------------------------------------------------------>缺少关键参数，无法正确执行
