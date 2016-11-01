b47ee132b7f734aca305434005ec7f2e19612b09
milobluebell <1444828173@qq.com>
2016-11-01 17:03:59 +0800

    就是为了更改项目的结构树而已。

--- js/controller/writeblogcontroller.js
+++ js/controller/writeblogcontroller.js
@@ -1,6 +1,7 @@
 /**
  * Created by 马子航 on 2016/4/15.
  */
+
 app.controller('writeblogcontroller',function ($scope, $rootScope, $location, checkStatus, $http, $q){
     var ueditor = UE.getEditor('ueditor-main'); //启用UEditor
     var InitArticleAid = $location.search()["aid"];//需要初始加入的文章的aid，一般在用户需要修改文章时需要
