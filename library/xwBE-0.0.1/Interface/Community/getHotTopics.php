9908eedaa85087905e06af9ca10109fa7fb59de2
mazih <123456>
2016-11-02 14:24:02 +0800

    增加了一个字段，重新修正了gitignore

--- library/xwBE-0.0.1/Interface/Community/getHotTopics.php
+++ library/xwBE-0.0.1/Interface/Community/getHotTopics.php
@@ -19,7 +19,7 @@ if($timing){
 
     $EchoResult = array();
 
-    $sql = "SELECT topic_id,title,topic_desc,par_times FROM `topics` ORDER BY par_times DESC LIMIT 5";
+    $sql = "SELECT topic_id,title,topic_desc,par_times,customed_url FROM `topics` ORDER BY par_times DESC LIMIT 5";
     $qry = $db->query($sql);
     $row_all = mysqli_num_rows($qry);
     if($row_all >= 1){
@@ -27,12 +27,12 @@ if($timing){
         while($row = $qry->fetch_assoc()){
 
             $result_topic_id = $row["topic_id"];
-            $result_par_times = $row["customed_url"];
+            $result_customed_url = $row["customed_url"];
             $result_title = $row["title"];
             $result_topic_desc = $row["topic_desc"];
             $result_par_times = $row["par_times"];
 
-            $dataArr = array('topic_id'=>$result_topic_id,'title'=>$result_title,'subtitle'=>$result_topic_desc,'readtimes'=>$result_par_times);
+            $dataArr = array('topic_id'=>$result_topic_id,'title'=>$result_title,'subtitle'=>$result_topic_desc,'customed_url'=>$result_customed_url,'readtimes'=>$result_par_times);
             foreach ( $dataArr as $key => $value ) {
                 $dataArr[$key] = urlencode ($value);
             }
