8d4d95a3687dbef871e3d35ee2fa8eec93813c67
milobluebell <1444828173@qq.com>
2016-12-28 13:39:00 +0800

    修改了getcovers接口，现在允许用官方封面文章来填充剩余的covers位置。

--- library/xwBE/Interface/Community/getCovers.php
+++ library/xwBE/Interface/Community/getCovers.php
@@ -18,8 +18,6 @@ $reminder = "";
 $b = new _environment();
 $tnow = $b->getTime();//当年的ISO时间
 $tnow_stamp = strtotime($tnow);//当前的UNIX时间
-$todayStart = mktime(0,0,0,date("m"),date("d"),date("Y"));  //今天0点UNIX时间
-$todayEnd = $todayStart + 24*60*60-1;                       //今天23:59的UNIX时间
 
 if($timing){
 
@@ -29,15 +27,6 @@ if($timing){
     @$qry = $db->query($sql);
     @$row_all = mysqli_num_rows($qry);
     if($row_all >= 1){
-
-        $RemainAmount = 4 - $row_all;
-        if($RemainAmount > 0){
-
-            $sql = "SELECT * FROM `covers` WHERE isAuth = '1' ORDER BY regtime DESC LIMIT $RemainAmount";//调取官方Cover
-            @$qry = $db->query($sql);
-            @$row_all = mysqli_num_rows($qry);
-
-        }
 
         while($row = $qry->fetch_assoc()){
 
@@ -57,6 +46,34 @@ if($timing){
             array_push($EchoResult,$dataArr);
 
         }
+
+        //判断在该发布时段内，是否少于4篇UGC封面文章：如果少于则用官方封面文章进行填充，反之则不填充。
+        $RemainAmount = 4 - $row_all;
+        if($RemainAmount > 0){
+
+            $sql1 = "SELECT * FROM `covers` WHERE isAuth = '1' ORDER BY regtime DESC LIMIT $RemainAmount";//调取官方Cover
+            @$qry1 = $db->query($sql1);
+            while($row1 = $qry1->fetch_assoc()){
+
+                $result_cover_id = $row["cover_id"];
+                $result_title = $row["title"];
+                $result_subtitle = $row["subtitle"];
+                $result_abstract = $row["abstract"];
+                $result_cover_img = $row["cover_img"];
+                $result_bg_color = $row["bg_color"];
+                $result_btn_content = $row["btn_content"];
+
+                $dataArr = array('cover_id'=>$result_cover_id,'title'=>$result_title,'subtitle'=>$result_subtitle,'abstract'=>$result_abstract,'cover_img'=>$result_cover_img,'bg_color'=>$result_bg_color,'btn_content'=>$result_btn_content);
+                foreach ( $dataArr as $key => $value ) {
+                    $dataArr[$key] = urlencode ($value);
+                }
+
+                array_push($EchoResult,$dataArr);
+
+            }
+
+        }
+        //End of 判断
 
         $EchoResult = urldecode ( json_encode ( $EchoResult ));
         echo $EchoResult;
